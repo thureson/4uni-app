722c0cb47f7b0d8f347c6bc4e5d5aafb0b195bc4
olli <oltsuwh@gmail.com>
2017-11-03 13:41:52 +0200

    Added event fetching

--- src/Events.js
+++ src/Events.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { View, Text } from "react-native";
 import { SuggestedEvents } from './SuggestedEvents.js';
 import { UpcomingEvents } from './UpcomingEvents.js';
+const api = "https://my-database.herokuapp.com/api/events";
 
 export class Events extends React.Component {
     constructor(props) {
         super(props);
         this.state = { upcoming: true };
         this.changeView = this.changeView.bind(this);
+    }
+
+    componentDidMount() {
+        fetch(api)
+        .then((response) => response.json())
+        .then((responseJson) => console.log(responseJson))
+        .catch((error) => {
+            console.log(error);
+        });
     }
 
     changeView() {
